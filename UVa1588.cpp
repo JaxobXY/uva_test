0700cb68be1e0653835bb3da47bca39a92bf1ada
JacobXY <JacobXY.email@email>
2025-08-13 11:02:00 -0700

    I just started on this but I don't know how to use maps.

---
+++ UVa1588.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    int counter;
+    int atem;
+    int btem;
+    vector <int> a;
+    vector <int> b;
+
+    while(1)
+    {
+        cin>atem
+    }
+}
