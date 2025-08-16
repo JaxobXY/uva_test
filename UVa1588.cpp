3ca2f7911614795d36d98558cdcfd2f7fdf8d0b0
JacobXY <JacobXY.email@email>
2025-08-15 18:34:49 -0700

    wrote the code for this, haven't submitted yet;

--- UVa1588.cpp
+++ UVa1588.cpp
@@ -1,18 +1,50 @@
 #include <iostream>
-#include <vector>
+#include <string>
+#include <climits>
 
 using namespace std;
 
-int main()
-{
-    int counter;
-    int atem;
-    int btem;
-    vector <int> a;
-    vector <int> b;
-
-    while(1)
-    {
-        cin>atem
+bool checkfit(const string &a, const string &b, int offset) {
+    for (int i = 0; i < b.size(); i++) {
+        if (offset + i >= 0 && offset + i < a.size()) {
+            if (a[offset + i] == '2' && b[i] == '2') {
+                return false;
+            }
+        }
     }
+    return true;
+}
+
+int main() {
+    string a, b;
+    cin >> a >> b;
+    int alen = a.size();
+    int blen = b.size();
+    int min_len = INT_MAX;
+
+    for (int offset = -blen + 1; offset < alen; offset++) {
+        if (checkfit(a, b, offset)) {
+            int start = min(0, offset);
+            int end = max(alen, offset + blen);
+            int current_len = end - start;
+            if (current_len < min_len) {
+                min_len = current_len;
+            }
+        }
+    }
+
+
+    for (int offset = -alen + 1; offset < blen; offset++) {
+        if (checkfit(b, a, offset)) {
+            int start = min(0, offset);
+            int end = max(blen, offset + alen);
+            int current_len = end - start;
+            if (current_len < min_len) {
+                min_len = current_len;
+            }
+        }
+    }
+
+    cout << min_len << endl;
+    return 0;
 }
