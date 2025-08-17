713a60c1913d1ccb67c9d12dcd62b55616e41622
JacobXY <JacobXY.email@email>
2025-08-16 21:50:18 -0700

    done

--- UVa1588.cpp
+++ UVa1588.cpp
@@ -4,11 +4,15 @@
 
 using namespace std;
 
-bool checkfit(const string &a, const string &b, int offset) {
-    for (int i = 0; i < b.size(); i++) {
+bool checkfit(const string &a, const string &b, int offset)
+{
+    for (int i = 0; i < b.size(); i++)
+    {
         int a_pos = offset + i;
-        if (a_pos >= 0 && a_pos < a.size()) {
-            if (a[a_pos] == '2' && b[i] == '2') {
+        if (a_pos >= 0 && a_pos < a.size())
+        {
+            if (a[a_pos] == '2' && b[i] == '2')
+            {
                 return false;
             }
         }
@@ -18,25 +22,32 @@ bool checkfit(const string &a, const string &b, int offset) {
 
 int main() {
     string a, b;
-    while (cin >> a >> b) {
+    while (cin >> a >> b)
+    {
         int alen = a.size();
         int blen = b.size();
         int min_len = INT_MAX;
 
         // Check all possible offsets (including negative)
-        for (int offset = -blen + 1; offset < alen; offset++) {
-            if (checkfit(a, b, offset)) {
+        for (int offset = -blen + 1; offset < alen; offset++)
+        {
+            if (checkfit(a, b, offset))
+            {
                 // Calculate the total length for this overlap
                 int start = min(0, offset);
                 int end = max(alen, offset + blen);
                 int length = end - start;
                 
-                if (length < min_len) {
+                if (length < min_len)
+                {
                     min_len = length;
                 }
             }
         }
-
+        if(min_len==INT_MAX)
+        {
+            min_len=alen+blen;
+        }
         cout << min_len << endl;
     }
     return 0;
