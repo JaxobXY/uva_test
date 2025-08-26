17b0256b7575f0e59be4450cc09ddb8e7ea40fdf
JacobXY <JacobXY.email@email>
2025-08-26 16:43:12 -0700

    Accepted

--- UVa 540.cpp
+++ UVa 540.cpp
@@ -1,56 +1,60 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>  // Added for find()
 using namespace std;
 
 int main() {
     int teamn;
-    cin >> teamn;
+    int sennum = 1;
     
-    // Create a vector for the main queue (will store the list of Ids in the line)
-    vector<int> mainQueue;
-    
-    // Create a 2D vector for teams: each team has its own queue of member IDs
-    vector<vector<int>> teams(teamn); // Initialize with 'teamn' empty teams
-    
-    for (int i = 0; i < teamn; i++) {
-        int teamSize;
-        cin >> teamSize; // Read how many members are in this team
+    while (cin >> teamn && teamn != 0) {
+        vector<int> mainQueue;
+        vector<vector<int>> teams(teamn);
         
-        for (int j = 0; j < teamSize; j++) {
-            int memberId;
-            cin >> memberId;
-            teams[i].push_back(memberId); // Add member to current team i
-        }
-    }
-    
-    string act;
-    int act2
-
-    while(cin>>act)
-    {
-        if (act=="0")
-        {
-            return 0;
-        }
-        else
-        {
-            cin>>act2;
-        }
-
-        if(act=="ENQUEUE")
-        {
+        // Create a mapping from member ID to team index
+        vector<int> memberToTeam(1000000, -1);
+        
+        for (int i = 0; i < teamn; i++) {
+            int teamSize;
+            cin >> teamSize;
             
-        }
-        else if(act=="DEQUEUE")
-        {
-
-        }
-        else if(stop)
-        {
-
+            for (int j = 0; j < teamSize; j++) {
+                int memberId;
+                cin >> memberId;
+                memberToTeam[memberId] = i;
+            }
         }
         
-
+        string act;
+        int act2;  // Moved declaration here
+        cout << "Scenario #" << sennum << endl;
+        sennum++;
+        
+        while (cin >> act && act != "STOP") {
+            if (act == "ENQUEUE") {
+                cin >> act2;
+                int teamIndex = memberToTeam[act2];
+                
+                if (find(mainQueue.begin(), mainQueue.end(), teamIndex) == mainQueue.end()) {
+                    mainQueue.push_back(teamIndex);
+                }
+                teams[teamIndex].push_back(act2);
+                
+            } else if (act == "DEQUEUE") {
+                if (!mainQueue.empty()) {
+                    int teamIndex = mainQueue.front();
+                    if (!teams[teamIndex].empty()) {
+                        cout << teams[teamIndex].front() << endl;
+                        teams[teamIndex].erase(teams[teamIndex].begin());
+                        
+                        if (teams[teamIndex].empty()) {
+                            mainQueue.erase(mainQueue.begin());
+                        }
+                    }
+                }
+            }
+        }
+        cout << endl;
     }
     
     return 0;
