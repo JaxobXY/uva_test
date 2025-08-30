7d09b3400d305a6ea517ff7e5e6172cdd9d498d3
JacobXY <JacobXY.email@email>
2025-08-30 15:55:34 -0700

    working on it

--- Uva 400.cpp
+++ Uva 400.cpp
@@ -9,27 +9,47 @@ int main()
 {
     vector <string> filenames;
     int nnames;
-    int maxlen=99999;
-    int res;
-    cin>>nnames;
+    int maxlen = 0;
+    cin >> nnames;
 
-    for(int i=0;i<nnames;i++)
+    for(int i = 0; i < nnames; i++)
     {
         string name;
-        cin>>name;
+        cin >> name;
         filenames.push_back(name);
+        if (name.length() > maxlen) {
+            maxlen = name.length();
+        }
     }
-    std::sort(filenames.begin(), filenames.end());
     
+    std::sort(filenames.begin(), filenames.end());
 
-
-
-        res=62/(maxlen+2);
-        cout<<res<<endl;
-
-    for(int i=filenames.size();i>0;i--)
-    {
-        cout<<filenames[i-1]<<endl;
+    // Calculate number of columns correctly
+    int cols = 1;
+    while (cols * maxlen + (cols - 1) * 2 <= 60) {
+        cols++;
     }
-    return 0;
+    cols--; // Step back to the last valid number of columns
+    
+    int rows = (nnames + cols - 1) / cols; // Ceiling division
+
+    cout << string(60, '-') << endl;
+
+    for(int i = 0; i < rows; i++)
+    {
+        for(int j = 0; j < cols; j++)
+        {
+            int index = i + j * rows;
+            if(index < filenames.size())
+            {
+                cout << filenames[index];
+                if (j < cols - 1) {
+                    // Add padding for all but last column
+                    int padding = maxlen + 2 - filenames[index].length();
+                    cout << string(padding, ' ');
+                }
+            }
+        }
+        cout << endl;
+    }
 }
