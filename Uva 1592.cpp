d972b0b205784cf65a1c58195096b238bde9be05
JacobXY <JacobXY.email@email>
2025-09-06 13:35:05 -0700

    done

--- Uva 1592.cpp
+++ Uva 1592.cpp
@@ -3,54 +3,73 @@
 #include <vector>
 #include <map>
 #include <utility>
-
+#include <sstream>
 using namespace std;
 
-int main()
-{
-    int x, y;
-    cin >> x >> y;
-    cin.ignore();
+int main() {
+    int n, m;
+    bool first_case = true;
     
-    vector<vector<string>> grid(x, vector<string>(y));
-    
-    for(int i = 0; i < x; i++)
-    {
-        string line;
-        getline(cin, line);
-        size_t start = 0;
-        for(int j = 0; j < y; j++)
-        {
-            size_t comma_pos = line.find(',', start);
-            if (comma_pos == string::npos) {
-                comma_pos = line.length();
+    while (cin >> n >> m) {
+        cin.ignore();
+        vector<vector<string>> grid(n, vector<string>(m));
+        
+        // Read the grid
+        for (int i = 0; i < n; i++) {
+            string line;
+            getline(cin, line);
+            
+            // Parse CSV line more carefully
+            stringstream ss(line);
+            string cell;
+            int j = 0;
+            
+            while (getline(ss, cell, ',') && j < m) {
+                grid[i][j] = cell;
+                j++;
             }
-            grid[i][j] = line.substr(start, comma_pos - start);
-            start = comma_pos + 1;
         }
-    }
-
-    // Check column pairs in order (smallest first)
-    for (int y1 = 0; y1 < y; y1++) {
-        for (int y2 = y1 + 1; y2 < y; y2++) {
-            map<pair<string, string>, int> seen;
-
-            for (int x1 = 0; x1 < x; x1++) {
-                pair<string, string> key = make_pair(grid[x1][y1], grid[x1][y2]);
-
-                if (seen.find(key) != seen.end()) {
-                    // Found violation
-                    cout << "NO" << endl;
-                    cout << seen[key] + 1 << " " << x1 + 1 << endl;  // rows
-                    cout << y1 + 1 << " " << y2 + 1 << endl;         // columns
-                    return 0;
-                } else {
-                    seen[key] = x1;
+        
+        bool found = false;
+        int found_x1 = -1, found_x2 = -1, found_y1 = -1, found_y2 = -1;
+        
+        // Check all pairs of columns
+        for (int y1 = 0; y1 < m && !found; y1++) {
+            for (int y2 = y1 + 1; y2 < m && !found; y2++) {
+                map<pair<string, string>, int> seen;
+                
+                for (int i = 0; i < n && !found; i++) {
+                    auto value_pair = make_pair(grid[i][y1], grid[i][y2]);
+                    
+                    if (seen.count(value_pair)) {
+                        // Found duplicate pair!
+                        found = true;
+                        found_x1 = seen[value_pair]; // first occurrence row
+                        found_x2 = i; // second occurrence row
+                        found_y1 = y1;
+                        found_y2 = y2;
+                    } else {
+                        seen[value_pair] = i; // store row index
+                    }
                 }
             }
         }
+        
+        // Output format
+        if (!first_case) {
+            cout << endl;
+        }
+        
+        if (found) {
+            cout << "NO" << endl;
+            cout << found_x1 + 1 << " " << found_x2 + 1 << endl;
+            cout << found_y1 + 1 << " " << found_y2 + 1 << endl;
+        } else {
+            cout << "YES" << endl;
+        }
+        
+        first_case = false;
     }
-
-    cout << "YES" << endl;
+    
     return 0;
 }
