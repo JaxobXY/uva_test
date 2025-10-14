1c4c9c38bb4be5cc5580689f022138ff4795930a
JacobXY <JacobXY.email@email>
2025-10-13 17:58:31 -0700

    done

--- aUSACO2016JanContestBronzeProb3.cpp
+++ aUSACO2016JanContestBronzeProb3.cpp
@@ -5,36 +5,38 @@
 
 using namespace std;
 
-struct pos
-{
-    int x;
-    int y;
-    vector<int> timeofentry;
-    int maxtimedistance;
-};
 
-char takemovement(char f, int j, int stepsbefore)
+
+
+char takemovement(char f, int j, int stepsbefore, int& x, int& y, int barf, int& timeofentry)
 {
     if(f=='N')
     {
-
+        for(int i=0;i<j;i++)
+        {
+            timeofentry[x][y]=barf - timeofentry[x][y]
+            y++;
+        }
     }
 }
 
 int main()
 {
+    int timeofentry[2001][2001];
     ifstream fin("mowing.in");
     ofstream fout("mowing.out");
     int n;
     fin >> n;
     int stepsbefore=0;
+    int curx = 1000;
+    int cury = 1000;
     for(int i=0;i<n;i++)
     {
         char x;
         int dis;
 
         fin >> x >> dis;
-        takemovement(x,dis);
+        takemovement(x,dis,curx,cury, stepsbefore, timeofentry[2001][2001]);
         stepsbefore++;
     }
 }
