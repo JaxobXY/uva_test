8809a2e5b6622b73163a2a6e86f5150a218480a8
JacobXY <JacobXY.email@email>
2025-10-14 18:57:57 -0700

    done with problem, also added a frame so I don't need to retype everything

--- aUSACO2016JanContestBronzeProb3.cpp
+++ aUSACO2016JanContestBronzeProb3.cpp
@@ -8,36 +8,69 @@ using namespace std;
 
 
 
-char takemovement(char f, int j, int stepsbefore, int& x, int& y, int barf, int& timeofentry)
+void takemovement(char direction, int distance, int& x, int& y, int& currentTime, int timeofentry[][2001], int& mindifference)
 {
-    if(f=='N')
+    for(int i = 0; i < distance; i++)
     {
-        for(int i=0;i<j;i++)
+        // 1. Move first
+        if(direction == 'N') y++;
+        else if(direction == 'S') y--;
+        else if(direction == 'E') x++;
+        else if(direction == 'W') x--;
+        
+        // 2. Increment time for this step
+        currentTime++;
+        
+        // 3. Check if we've been here before
+        if(timeofentry[x][y] != 0) 
         {
-            timeofentry[x][y]=barf - timeofentry[x][y]
-            y++;
+            // This position was visited before
+            int timeDiff = currentTime - timeofentry[x][y];
+            // 4. Update mindifference if this is the first difference or smaller
+            if(mindifference == 0 || timeDiff < mindifference) 
+            {
+                mindifference = timeDiff;
+            }
         }
+        
+        // 5. Record current time for this position
+        timeofentry[x][y] = currentTime;
     }
 }
 
 int main()
 {
-    int timeofentry[2001][2001];
+    int timeofentry[2001][2001] = {0};
+    int mindifference = 0;  // Start at 0, meaning no crossing found
     ifstream fin("mowing.in");
     ofstream fout("mowing.out");
     int n;
     fin >> n;
-    int stepsbefore=0;
+    int currentTime = 0;  // Better name
     int curx = 1000;
     int cury = 1000;
-    for(int i=0;i<n;i++)
+    
+    // Record the starting position at time 0
+    timeofentry[curx][cury] = 0;
+    
+    for(int i = 0; i < n; i++)
     {
-        char x;
-        int dis;
-
-        fin >> x >> dis;
-        takemovement(x,dis,curx,cury, stepsbefore, timeofentry[2001][2001]);
-        stepsbefore++;
+        char direction;
+        int distance;
+        fin >> direction >> distance;
+        
+        takemovement(direction, distance, curx, cury, currentTime, timeofentry, mindifference);
+        // Remove the stepsbefore++ since currentTime is updated in the function
     }
-
+    
+    // If no crossing was found, output -1
+    if(mindifference == 0) {
+        fout << -1 << endl;
+    } else {
+        fout << mindifference << endl;
+    }
+    
+    fin.close();
+    fout.close();
+    return 0;
 }
---
+++ frameworkforusaco.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <map>
+
+using namespace std;
+
+
+
+int main()
+{
+    int n;
+    ifstream fin("");
+    ofstream fout("");
+    fin >> n;
+    fin.close();
+    fout.close();
+    return 0;
+
+}
