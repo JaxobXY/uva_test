a5c13d76db968178a339b89877f7d44929c65c99
JacobXY <JacobXY.email@email>
2025-12-06 10:31:40 -0800

    done

---
+++ aUSACO2016USOpenContestSilverProb1.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <map>
+#include <algorithm>
+#include <set>
+
+using namespace std;
+
+
+
+int main()
+{
+    int n;
+    ifstream fin("reduce.in");
+    ofstream fout("reduce.out");
+    fin >> n;
+    fin.close();
+    fout.close();
+    return 0;
+
+}
