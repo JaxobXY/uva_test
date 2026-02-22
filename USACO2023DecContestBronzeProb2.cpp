667585dcc699e4a3665892684cedac9f12e8d6ef
JacobXY <JacobXY.email@email>
2026-02-22 12:30:33 -0800

    done

--- USACO2023DecContestBronzeProb2.cpp
+++ USACO2023DecContestBronzeProb2.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 #include <vector>
 #include <utility>
 
@@ -11,199 +12,127 @@ int main()
 {
     ios::sync_with_stdio(0); cin.tie(0);
     int n;
-    
+    string cows;
     cin >> n;
-    vector<int> cow(n);
-    vector<pair<bool,pair<int,int>>> groups;
-    vector<int> maxdays;
-    int l = -1;
-    int r;
-    int minmaxday = 9999999;
-    int day = 0;
+    cin >> cows;
+    int counter = 0;
+    bool edge = true;
+    vector<pair<int,bool>> groups;
+    int minmaxdays = 150001;
     for(int i=0;i<n;i++)
     {
-        char tem;
-        cin >> tem;
-        if(tem == '0')
+        int temp;
+        if(cows[i] == '0')
         {
-            cow[i] = 0;
-        }
-        else
-        {
-            cow[i] = 1;
-        }
-        if(cow[i] == 1 && l == -1)
-        {
-            l=i;
-            r=-2;
-        }
-        if(cow[i] == 0 && l!=-1)
-        {
-            day = 1;
-            r=i-1;
-            if(r==l)
+            temp = 0;
+            if(i == 0)
             {
-                maxdays.push_back(1);
-                if(day<minmaxday)
-                {
-                    minmaxday=day;
-                }
-                l=-1;
-                r=-1;
+                edge = false;
                 continue;
             }
-            groups.push_back(make_pair(l == 0 || r == n-1, make_pair(l,r)));
-            if(l == 0 || r == n-1)
+            if(counter !=0)
             {
-                day += r-l+1;
-            }
-            else if(r-l+1 == 1 || r-l+1 == 2)
-            {
-                maxdays.push_back(1);
-                day += 1;
-            }
-            else
-            {
-                int temp = 0;
-                if((r-l-1)%2!=0)
+                int days;
+                if(edge == true)
                 {
-                    temp = 1;
+                    days=counter;
                 }
-                day += (r-l-1+temp)/2;
+                else if(counter < 3)
+                {
+                    days = counter;
+                }
+                else if(counter%2 == 1)
+                {
+                    days = (counter-1)/2+1;
+                }
+                else
+                {
+                    days = counter/2;
+                }
+
+                // printf("Found group of %d cows, calculated max days %d. [On edge %d]\n", 
+                    // counter, days, edge);
+
+                if(days<minmaxdays)
+                {
+                    minmaxdays=days;
+                    // printf("Upate min maxdays to %d\n", minmaxdays);
+                }
+                groups.push_back(make_pair(counter,edge));
+                counter = 0;
+                edge = false;
             }
-            maxdays.push_back(day);
-            if(day<minmaxday)
-            {
-                minmaxday=day;
-            }
-            l=-1;
-            r=-1;
-        }
-    }
-    day = 0;
-    if(r==-2)
-    {
-        r=n-1;
-        groups.push_back(make_pair(l == 0 || r == n-1, make_pair(l,r)));
-        if(r==l)
-        {
-            maxdays.push_back(1);
-            if(day<minmaxday)
-            {
-                minmaxday=1;
-            }
-            l=-1;
-            r=-1;
-            goto skip;
-        }
-        if(l == 0 || r == n-1)
-        {
-            maxdays.push_back(r-l+1);
-            day += r-l+1;
-        }
-        else if(r-l+1 == 1 || r-l+1 == 2)
-        {
-            maxdays.push_back(1);
-            day += 1;
         }
         else
         {
-            int temp = 0;
-            if((r-l-1)%2!=0)
             temp = 1;
-            maxdays.push_back((r-l-1+temp)/2);
-            day += (r-l-1+temp)/2;
-        }
-        if(day<minmaxday)
-        {
-            minmaxday=day;
+            counter+=1;
         }
     }
-    skip:
+    if(counter>0)
+    {
+        groups.push_back(make_pair(counter,true));
+        int days;
+
+        days=counter;
+        // printf("Last group, %d cows, calculated max days %d\n", counter, days);
+        if(days<minmaxdays)
+        {
+            minmaxdays=days;
+            // printf("Upate min maxdays to %d\n", minmaxdays);
+        }
+    }
+
+
+
     int total = 0;
-    if(groups.size()==0)
-    {
-        cout << 0;
-        return 0;
-    }
-    if(groups.size() == 1)
-    {
-        if((groups[0].second.second-groups[0].second.first+1)%2 == 0)
-        {
-            if(groups[0].second.second == n-1 && groups[0].second.first==0)
-            {
-                cout << 1;
-                return 0;
-            }
-            cout << 2;
-            return 0;
-        }
-        else
-        {
-            cout << 1;
-            return 0;
-        }
-
-    }
-
-
     for(int i=0;i<groups.size();i++)
     {
 
-        if(groups[i].first == true)
+        int len = groups[i].first;
+        bool edge = groups[i].second;
+        if(edge)
         {
-            int len = groups[i].second.second-groups[i].second.first+1;
-            int cows = 0;
-            if(minmaxday<=len && len%2 == 0 && minmaxday>=(len-2)/2+2)
+            if(len == n)
             {
                 total+=1;
+                // cout << "group " << i << " everything is one and length is" << n<< endl;
                 continue;
-            }
-            else if(minmaxday<=len && len%2 != 0 && minmaxday>=(len-1)/2+1)
-            {
-                total+=1;
-                continue;
-            }
-            cows = len/(minmaxday*2+1);
-            if(len%(minmaxday*2+1) != 0)
-            {
-                cows+=1;
-            }
-            total+=cows;
-        }
-        else
-        {
-            int len = groups[i].second.second-groups[i].second.first+1;
-            int cows = 0;
-            if(len%2 == 0)
-            {
-                cows+=2;
-                len-=(2+4*(minmaxday));
-                if(len>0)
-                {
-                    cows+=len/(1+2*minmaxday);
-                    if(len%(1+2*minmaxday) > 0)
-                    {
-                        cows+=1;
-                    }
-                }
-                total+=cows;
             }
             else
             {
-                cows+=1;
-                if(len-(1+2*minmaxday)>0)
+                if((minmaxdays-1)*2+1>=len)
                 {
-                    cows+=(len-(1+2*minmaxday))/(1+2*minmaxday);
-                    if((len-(1+2*minmaxday))%(1+2*minmaxday)>0)
-                    {
-                        cows+=1;
-                    }
+                    total +=1;
+                    // cout << "group " << i << " 1 cow can cover everything" << endl;
+                    continue;
                 }
-                total+=cows;
+                else
+                {
+                    int temp;
+                    int tem = (minmaxdays-1)*2+1;
+                    temp = (len+tem-1)/(tem);
+                    total+=temp;
+                    // cout << "group " << i << " 1 cow can cover" << tem << "and need " << temp << "cows" << endl;
+                    continue;
+                }
             }
         }
+        int temp;
+        int tem = (minmaxdays-1)*2+1;
+        temp = (len+tem-1)/(tem);
+        // printf(
+            // "Group %d : %d cows, 1 cow can cover %d, calculated to need %d cows\n", 
+            // i, len, tem, temp);
+         
+        if(temp == 1 && len%2==0)
+        {
+            temp +=1;
+            // printf("Adjusted to need %d cows\n", temp);
+        }
+        total+=temp;
     }
+    // printf("Total at least need %d cows\n", total);
     cout << total;
     return 0;
 
