460f1940fa037ea9cc763e2a7340cfd320a889e2
JacobXY <JacobXY.email@email>
2026-03-08 12:14:04 -0700

    Latest changes
    Signed-off-by: JacobXY <JacobXY.email@email>

--- past problems/USACO2024FebContestBronzeProb2revised.cpp
+++ past problems/USACO2024FebContestBronzeProb2revised.cpp
@@ -15,51 +15,59 @@ int main()
     ios::sync_with_stdio(0); cin.tie(0);
     int64 n;
     int64 t;
-    ifstream fin("input.txt");
-    fin >> n >> t;
+    //ifstream fin("input.txt");
+    cin >> n >> t;
     deque<pair<char,int64>> input(n,{'A',0}); 
     string inputs;
-    fin >> inputs;
+    cin >> inputs;
     int64 total = 0;
     for(int64 i=0;i<n;i++)
     {
         input[i].first = inputs[i];
-        fin >> input[i].second;
-        cout << "old total: " << total;
+        cin >> input[i].second;
+        //cout << "old total: " << total;
         total+=input[i].second;
-        cout << " total += " << input[i].second << " new total: " << total << endl;
+        //cout << " total += " << input[i].second << " new total: " << total << endl;
     }
     long long counter = 0;
-    while(counter<n && inputs[counter]=='L')
-    {
-        if (!input.empty())
+
+    if(counter<n && inputs[counter]=='L')
+    {        
+
+        while(counter<n && inputs[counter]=='L')
         {
-            pair<char,int64> val = input.front();   // Get the first element
-            input.pop_front();          // Remove it from the front
-            input.push_back(val);       // Add it to the back
-        }   
-        counter++;
-    }
-    if(counter==n)
-    {
-        cout << total << endl;
-        return 0;
-    }
-    counter = n-1;
-    while(counter>=0 && inputs[counter]=='R')
-    {
-        if (!input.empty())
+            if (!input.empty())
+            {
+                pair<char,int64> val = input.front();   // Get the first element
+                input.pop_front();          // Remove it from the front
+                input.push_back(val);       // Add it to the back
+            }   
+            counter++;
+        }
+        if(counter==n)
         {
-            pair<char,int64> val = input.back();   // Get the first element
-            input.pop_back();          // Remove it from the front
-            input.push_front(val);       // Add it to the back
-        }   
-        counter--;
+            cout << total << endl;
+            return 0;
+        }
     }
-    if(counter==(-1))
+    else
     {
-        cout << total << endl;
-        return 0;
+        counter = n-1;
+        while(counter>=0 && inputs[counter]=='R')
+        {
+            if (!input.empty())
+            {
+                pair<char,int64> val = input.back();   // Get the first element
+                input.pop_back();          // Remove it from the front
+                input.push_front(val);       // Add it to the back
+            }   
+            counter--;
+        }
+        if(counter==(-1))
+        {
+            cout << total << endl;
+            return 0;
+        }
     }
     int64 output = total;
     if(n<=2)
