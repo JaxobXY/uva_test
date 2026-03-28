fd419121aa3514bd0e694c758b97f485077a6946
JacobXY <JacobXY.email@email>
2026-03-27 21:25:28 -0700

    still working on this thing

--- USACO2024USOpenContestBronzeProb3.cpp
+++ USACO2024USOpenContestBronzeProb3.cpp
@@ -25,33 +25,141 @@ int main()
         int n;
         cin >> n;
         vector<int> list(n,0);
-        for(int j=0;j<n;j++)
+        vector<int> count(n+1,0);
+        for(int j=0;j<n-1;j++)
         {
             cin >> list[j];
+            count[list[j]]+=1;
+            cout << "cin >> " << list[j] << "count[ "<< list[j] << "]+=1" << endl;
         }
-        deque<int> recreate;
-        recreate.push_back(list[n-1]);
-        recreate.push_back(list[n-2]);
-        bool rightmrecent = true;
-        deque<int> recreatel;
-        deque<int> recreater;
+        for(int j=1;j<=n;j++)
+        {
+            if(j==1)
+            {
+                if(count[j]<1 || count[j]>2)
+                {
+                    cout << -1 << endl;
+                    cout << "too many ones or no ones" << count[1] << endl;
+                    continue;
+                }
+                else if(count[j]>1)
+                {
+                    cout << -1 << endl;
+                    cout << "too many " << count[j] << "s" << endl;
+                    continue;
+                }
+            }
+        }
+        if(list[n-1]!=0)
+        {
+            cout << -1 << endl;
+            continue;
+        }
+        //is recently placed side right ???
+        bool justabool=true;
+        deque<int> rebuild;
+        int smallest[2]={0,0};
+        int small=0;
+        for(int j=1;j<=n;j++)
+        {
+            if(count[j]==0 && smallest[0]==0)
+            {
+                smallest[0]=j;
+            }
+            else if(count[j]==0 && smallest[0]!=0)
+            {
+                smallest[0]=j;
+                break;
+            }
+        }
+        rebuild.push_back(list[n-2]);
+        if(n==2)
+        {
+            cout << list[0] << endl;
+            continue;
+        }   
         for(int j=n-3;j>=0;j--)
         {
-            recreatel=recreate;
-            recreater=recreate;
-            recreatel.push_front(list[j]);
-            recreater.push_back(list[j]);
-            bool retur = false;
-            if(recreatel[0]>recreatel[recreatel.size()-1] && rightmrecent=true)
+            if(list[j]==1)
             {
-                retur=true;
+                if(rebuild.back()==1)
+                {
+                    rebuild.push_back(list[j-1]);
+                    justabool=true;
+                }
+                else if(rebuild.front()==1)
+                {
+                    rebuild.push_front(list[j-1]);
+                    justabool=false;
+                }
+                else
+                {
+                    cout << -1 << endl;
+                    continue;
+                }
             }
-            if(recreatel.front<recreatel.back && rightmrecent=false)
+            else if(rebuild.size()==1)
             {
-                retur=true;
+                rebuild.push_back(list[n-3]);
+                justabool=true;
+            }
+            else
+            {
+                if(rebuild.back()==list[j] || justabool)
+                {
+                    rebuild.push_back(list[j-1]);
+                    justabool=true;
+                }
+                else if(rebuild.front()==list[j] || !justabool)
+                {
+                    rebuild.push_front(list[j-1]);
+                    justabool=false;
+                }
+                else
+                {
+                    cout << -1 << endl;
+                    continue;
+                }
+            }
+        }
+        if(justabool)
+        {
+            rebuild.push_back(smallest[0]);
+            justabool=false;
+        }
+        else if(!justabool)
+        {
+            rebuild.push_front(smallest[0]);
+            justabool=true;
+        }
+        if(smallest[1]!=0)
+        {
+            if(justabool)
+            {
+                rebuild.push_back(smallest[1]);
+                justabool=true;
+            }
+            else if(!justabool)
+            {
+                rebuild.push_front(smallest[1]);
+                justabool=false;
             }
         }
 
+
+
+        for(int j=0;j<rebuild.size();j++)
+        {
+            cout << rebuild[j];
+            if(j!=rebuild.size()-1)
+            {
+                cout << " ";
+            }
+            else
+            {
+                cout << endl;
+            }
+        }
     }
     
     return 0;
