b04d885a480bba7b7a7a102a643ca9859e162e45
JacobXY <JacobXY.email@email>
2026-04-09 19:18:44 -0700

    started

--- USACO2024USOpenContestBronzeProb3.cpp
+++ USACO2024USOpenContestBronzeProb3.cpp
@@ -1,13 +1,7 @@
 #include <iostream>
 #include <vector>
-#include <string>
-#include <map>
-#include <algorithm>
 #include <set>
-#include <utility>
-#include <unordered_set>
-#include <deque>
-#include <climits>
+#include <numeric>
 
 using namespace std;
 #define int64 long long
@@ -16,6 +10,8 @@ using namespace std;
 int main()
 {
     ios::sync_with_stdio(0); cin.tie(0);
+    vector <int> temporary(200001);
+    iota(temporary.begin(),temporary.end(),0);
     int t;
     
     cin >> t;
@@ -24,142 +20,45 @@ int main()
     {
         int n;
         cin >> n;
-        vector<int> list(n,0);
-        vector<int> count(n+1,0);
-        for(int j=0;j<n-1;j++)
+        vector <int> list(n,0);
+        //count of number 1
+        int count = 0;
+        set <int> missing;
+        missing.insert(temporary.begin()+1, temporary.begin()+n+1);
+        bool condition=true;
+        for(int j=0;j<n;j++)
         {
             cin >> list[j];
-            count[list[j]]+=1;
-            cout << "cin >> " << list[j] << "count[ "<< list[j] << "]+=1" << endl;
-        }
-        for(int j=1;j<=n;j++)
-        {
-            if(j==1)
+            count+=(list[j]==1);
+            auto it = missing.find(list[j]);
+            if (it != missing.end()) {
+                missing.erase(it);
+            }
+            else if(list[j]=1 && count==2)
             {
-                if(count[j]<1 || count[j]>2)
-                {
-                    cout << -1 << endl;
-                    cout << "too many ones or no ones" << count[1] << endl;
-                    continue;
-                }
-                else if(count[j]>1)
-                {
-                    cout << -1 << endl;
-                    cout << "too many " << count[j] << "s" << endl;
-                    continue;
-                }
+            }
+            else//too much of any number. 
+            {
+                condition = false;
             }
         }
-        if(list[n-1]!=0)
+        // too little 1's. too much covered below
+        if(count=0)
         {
-            cout << -1 << endl;
+            goto cont;
+        }
+        //too many of a number including 1
+        if(condition = false)
+        {
+            goto cont;
+        }
+        if(0)
+        {
+            cont:
+            cout << i << -1 << endl;
             continue;
         }
-        //is recently placed side right ???
-        bool justabool=true;
-        deque<int> rebuild;
-        int smallest[2]={0,0};
-        int small=0;
-        for(int j=1;j<=n;j++)
-        {
-            if(count[j]==0 && smallest[0]==0)
-            {
-                smallest[0]=j;
-            }
-            else if(count[j]==0 && smallest[0]!=0)
-            {
-                smallest[0]=j;
-                break;
-            }
-        }
-        rebuild.push_back(list[n-2]);
-        if(n==2)
-        {
-            cout << list[0] << endl;
-            continue;
-        }   
-        for(int j=n-3;j>=0;j--)
-        {
-            if(list[j]==1)
-            {
-                if(rebuild.back()==1)
-                {
-                    rebuild.push_back(list[j-1]);
-                    justabool=true;
-                }
-                else if(rebuild.front()==1)
-                {
-                    rebuild.push_front(list[j-1]);
-                    justabool=false;
-                }
-                else
-                {
-                    cout << -1 << endl;
-                    continue;
-                }
-            }
-            else if(rebuild.size()==1)
-            {
-                rebuild.push_back(list[n-3]);
-                justabool=true;
-            }
-            else
-            {
-                if(rebuild.back()==list[j] || justabool)
-                {
-                    rebuild.push_back(list[j-1]);
-                    justabool=true;
-                }
-                else if(rebuild.front()==list[j] || !justabool)
-                {
-                    rebuild.push_front(list[j-1]);
-                    justabool=false;
-                }
-                else
-                {
-                    cout << -1 << endl;
-                    continue;
-                }
-            }
-        }
-        if(justabool)
-        {
-            rebuild.push_back(smallest[0]);
-            justabool=false;
-        }
-        else if(!justabool)
-        {
-            rebuild.push_front(smallest[0]);
-            justabool=true;
-        }
-        if(smallest[1]!=0)
-        {
-            if(justabool)
-            {
-                rebuild.push_back(smallest[1]);
-                justabool=true;
-            }
-            else if(!justabool)
-            {
-                rebuild.push_front(smallest[1]);
-                justabool=false;
-            }
-        }
-
-
-
-        for(int j=0;j<rebuild.size();j++)
-        {
-            cout << rebuild[j];
-            if(j!=rebuild.size()-1)
-            {
-                cout << " ";
-            }
-            else
-            {
-                cout << endl;
-            }
-        }
+        else{cout << i << "im good" << endl;}
     }
     
     return 0;
---
+++ asdfgarsd.cpp
@@ -0,0 +1,113 @@
+#include <iostream>
+#include <vector>
+#include <numeric>   // for std::iota
+#include <algorithm> // for std::shuffle
+#include <random>    // for std::mt19937 and std::random_device
+#include <list>
+
+using namespace std;
+int main() {
+    int n;
+    cout << "how many times(this is 1 in the middle probably)" << endl;
+    cin >> n;
+    for(int j=0;j<n;j++)
+    {
+        // 1. Create a list (vector) with 10 elements
+        list<int> listt;
+        vector<int> list(10);
+        
+        // 2. Fill it with numbers 1 to 10
+        iota(list.begin(), list.end(), 1);
+
+        // 3. Set up a random number generator
+        random_device rd; 
+        mt19937 g(rd());
+
+        // 4. Shuffle the list
+        shuffle(list.begin(), list.end(), g);
+
+        for(int i=0;i<10;i++)
+        {
+            listt.push_back(list[i]);
+        }
+        while(listt.size()>1)
+        {
+            if(listt.front()>listt.back())
+            {
+                listt.pop_front();
+                cout << listt.front() << " ";
+            }
+            else
+            {
+                listt.pop_back();
+                cout << listt.back() << " ";
+            }
+        }
+        cout << endl;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    cout << "how many times(this is 1 in the edge)" << endl;
+    cin >> n;
+    for(int j=0;j<n;j++)
+    {
+        // 1. Create a list (vector) with 10 elements
+        list<int> listt;
+        vector<int> list(10);
+        
+        // 2. Fill it with numbers 1 to 10
+        iota(list.begin(), list.end(), 1);
+
+        // 3. Set up a random number generator
+        random_device rd; 
+        mt19937 g(rd());
+
+        // 4. Shuffle the list
+        shuffle(list.begin()+1, list.end(), g);
+
+        for(int i=0;i<10;i++)
+        {
+            listt.push_back(list[i]);
+        }
+        while(listt.size()>1)
+        {
+            if(listt.front()>listt.back())
+            {
+                listt.pop_front();
+                cout << listt.front() << " ";
+            }
+            else
+            {
+                listt.pop_back();
+                cout << listt.back() << " ";
+            }
+        }
+        cout << endl;
+    }
+    return 0;
+}
